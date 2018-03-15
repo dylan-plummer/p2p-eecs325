a47a255ee2654c4a7877027f99da1117feaef65c
jumproper <dylanmplummer@gmail.com>
2018-03-15 13:28:48 -0400

    Add start of file query

--- p2p/p2p/src/Main.java
+++ p2p/p2p/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,14 +8,21 @@ public class Main {
     private static final int END_PORT = 50619;
     private static boolean running = true;
 
-    private static ArrayList<String> addressList;
-    private static ArrayList<Socket> peerConnections = new ArrayList<>();
+    private static String fileName;
+    public static ArrayList<String> addressList;
+    public static ArrayList<Socket> peerConnections = new ArrayList<>();
 
     public static void main(String[] args){
         addressList = fillAddresses("config_neighbors.txt");
         System.out.println(addressList.toString());
+        Thread getFile = new Thread(fileName);
+            for(Socket socket:peerConnections){
+                if(queryPeer(fileName,socket)){
+
+                }
+            }
         while(running){
-            runCommand(getCommand());
+            runCommand(getCommand()); //run user input
         }
     }
 
@@ -83,5 +88,18 @@ public class Main {
         disconnectFromPeers();
         running = false;
         System.out.println("Exiting...");
+    }
+
+    public static boolean queryPeer(String fileName, Socket socket){
+        int qId = (int)(Math.random() * 10000);
+        DataOutputStream  outToClient;
+        try {
+            outToClient = new DataOutputStream(socket.getOutputStream());
+            String queryMessage = "Q:" + qId + ";" + fileName;
+            outToClient.writeBytes(queryMessage);
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream())); //peer will query its neighbors if it does not have the file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
